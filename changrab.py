cafdf35302509c3cdd819b6ab283e4c5ff90445a
SamNi <sgni@fastmail.fm>
2015-10-25 00:37:01 -0400

    Initial import into git

---
+++ changrab.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import random as rnd
+import math as m
+import re
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ changrab_test.py
@@ -0,0 +1,17 @@
+import unittest
+import changrab as cgr
+
+class changrabTest(unittest.TestCase):
+    def setUp(this):
+        pass
+    def tearDown(this):
+        pass
+    def test_X(this):
+        raise NotImplementedError 
+    def test_Y(this):
+        raise NotImplementedError 
+    def test_Z(this):
+        raise NotImplementedError 
+
+if __name__ == '__main__':
+    unittest.main()
