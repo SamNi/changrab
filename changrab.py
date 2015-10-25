e0c8aabff9fc04fa2c059475f57c6a6016af9f81
SamNi <sgni@fastmail.fm>
2015-10-25 03:06:30 -0400

    Crude rate limiting for politeness, sloppy exception non-handling, handle filename extensions correctly

--- changrab.py
+++ changrab.py
@@ -3,6 +3,8 @@ from os import path
 import sys
 import json
 import re
+
+from time import sleep
 from urllib.request import urlopen, urlretrieve
 from urllib.parse import urlparse
 
@@ -17,10 +19,10 @@ def get_thread_image_paths(url):
     path = r'http://a.4cdn.org/%s/thread/%s.json' % (board, thread_id)
     json_response = json.loads( urlopen(path).readall().decode() )
     posts = json_response['posts']
-    image_paths = set([])
-    for fname in [ p['tim'] for p in posts if 'filename' in p]:
-        filepath = r'http://i.4cdn.org/%s/%s.jpg' % (board, fname)
-        image_paths.add(filepath)
+    image_paths = []
+    for (fname, fext) in [ (p['tim'], p['ext']) for p in posts if 'filename' in p]:
+        filepath = r'http://i.4cdn.org/%s/%s%s' % (board, fname, fext)
+        image_paths.append(filepath)
     return board, thread_id, image_paths
 
 def main():
@@ -28,15 +30,13 @@ def main():
     board, thread_id, image_paths = get_thread_image_paths(inp_url)
     filecount = 0
     for imgpath in image_paths:
-        try:
-            localfname = path.basename(urlparse(imgpath)[2])
-            localpath = path.join(os.path.curdir, board, thread_id, localfname)
-            os.makedirs(path.dirname(localpath), exist_ok=True)
-            sys.stdout.write(imgpath + '\t\t')
-            urlretrieve(imgpath, localpath)
-            sys.stdout.write('done!\n')
-            filecount += 1
-        except:
+        localfname = path.basename(urlparse(imgpath)[2])
+        localpath = path.join(os.path.curdir, board, thread_id, localfname)
+        os.makedirs(path.dirname(localpath), exist_ok=True)
+        sys.stdout.write(imgpath + '\t\t')
+        urlretrieve(imgpath, localpath)
+        sys.stdout.write('successful\n')
+        filecount += 1
     print("Scraped %d image(s)" % filecount)
 
 if __name__ == '__main__':
